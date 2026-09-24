828c8d7214ed165077971cc230b6d2109813396c
samirsarkardk <samirsarkar411411@gmail.com>
2026-09-25 05:03:07 +0530

    m

--- Bmodel.py
+++ Bmodel.py
@@ -65,3 +65,34 @@ class PINN2(nn.Module):
         inputs = torch.cat((x,t), dim= 1)
 
         return self.network(inputs)
+
+
+class PINN3(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        layers = []
+
+        # Input layer
+
+        layers.append(nn.Linear(config.input_dim, config.hidden_dim))
+        layers.append(nn.Tanh())
+
+        # Hidden layers
+
+        for _ in range(config.num_hidden_layers -1 ):
+            layers.append(nn.Linear(config.hidden_dim, config.hidden_dim))
+            layers.append(nn.Tanh())
+
+
+        # output layer
+
+        layers.append(nn.Linear(config.hidden_dim, config.output_dim))
+
+        self.network = nn.Sequential(*layers)
+
+
+    def forward(self, x, t):
+        inputs = torch.cat((x,t), dim= 1)
+
+        return self.network(inputs)
